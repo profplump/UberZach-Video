06f78a94d404cd27696414d1bfcd86bf962d2bf2
Zachary Kotlarek <zach@kotlarek.com>
2014-04-27 09:08:01 -0700

    One at a time

--- yt/update.sh
+++ yt/update.sh
@@ -4,16 +4,11 @@
 MEDIA_PATH="`~/bin/video/mediaPath`"
 
 # Check and write the run file
-PID_FILE="${TMPDIR}/ytUpdate.pid"
+PID_FILE="/tmp/ytUpdate.pid"
 if [ -f "${PID_FILE}" ]; then
 	PID=`cat "${PID_FILE}"`
 	if ps auwx | grep -v grep | grep "`basename "${0}"`" | grep -q "${PID}"; then
-		if [ $DEBUG -gt 0 ]; then
-			echo "Already running: ${PID}" 1>&2
-			exit -1
-		else
 			exit 0
-		fi
 	fi
 fi
 echo $$ > "${PID_FILE}"
