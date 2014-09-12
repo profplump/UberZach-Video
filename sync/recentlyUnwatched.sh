76ee4c1a8c21e911531f06a4c94a4c14ed09ace5
Zachary Kotlarek <zach@kotlarek.com>
2014-09-11 17:44:28 -0700

    Tweak to fit available space on Beddy

--- sync/recentlyUnwatched.sh
+++ sync/recentlyUnwatched.sh
@@ -2,8 +2,8 @@
 
 HOST="http://pms.uberzach.com:32400"
 CURL_OPTS=(--silent --connect-timeout 5 --max-time 30)
-NUM_SERIES=20
-NUM_EPISODES=5
+NUM_SERIES=18
+NUM_EPISODES=4
 MAX_RESULTS=100
 
 # Select a configuration mode
