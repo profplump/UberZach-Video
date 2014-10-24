1b05ef5901316492bea66d969ef4687e396e67d5
Zachary Kotlarek <zach@kotlarek.com>
2014-10-23 18:55:40 -0700

    Support linux

--- checkLoad.sh
+++ checkLoad.sh
@@ -2,7 +2,11 @@
 
 # Bail if the load average is high
 LOAD="`uptime | awk -F ': ' '{print $2}' | cut -d '.' -f 1`"
-CPU_COUNT="`sysctl -n hw.ncpu`"
+if [ -r /proc/cpuinfo ]; then
+	CPU_COUNT="`cat /proc/cpuinfo | awk '$1 == "processor" {print $3}' | sort | tail -n 1`"
+else
+	CPU_COUNT="`sysctl -n hw.ncpu`"
+fi
 if [ $LOAD -gt $(( 2 * $CPU_COUNT )) ]; then
 	exit 1
 fi
