6c581dcce788fb2d14d4b1d9d0a370913aa46af1
Zachary Kotlarek <zach@kotlarek.com>
2014-10-24 05:04:52 -0700

    Provide a script to kick the WebDAV mount back into shape when it fails

---
+++ backup/checkDrive.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Config
+if [ -z "${BASE_REMOTE}" ]; then
+	BASE_REMOTE="/mnt/remote/opendrive"
+fi
+WC="${BASE_REMOTE}/.live_check"
+
+# State
+FAILED=0
+
+# Check
+touch "${WC}" >/dev/null 2>&1
+if [ ! -e "${WC}" ]; then
+	FAILED=1
+fi
+
+# Re-check (and cleanup)
+rm -f "${WC}" >/dev/null 2>&1
+if [ $FAILED -lt 1 ]; then
+	if [ -e "${WC}" ]; then
+		FAILED=1
+	fi
+fi
+
+# Reset if needed
+if [ $FAILED -gt 0 ]; then
+	SUOD=""
+	if [ $UID -gt 0 ]; then
+		SUDO="sudo"
+	fi
+
+	DASH_NAME="`echo "${BASE_REMOTE}" | cut -d '/' -f 2- | sed 's%/%-%g'`"
+	PID=`ps a -o pid=,command= | grep "${BASE_REMOTE}" | awk '{print $1}'`
+	if [ -n "${PID}" ] && [ $PID -gt 1 ]; then
+		${SUDO} kill -9 $PID
+		${SUDO} umount "${BASE_REMOTE}"
+		${SUDO} rm "/var/run/mount.davfs/${DASH_NAME}.pid"
+		${SUDO} mount "${BASE_REMOTE}"
+	fi
+fi
