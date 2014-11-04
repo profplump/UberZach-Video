99f3e552927c5587986fc4108509c0db0bdeee5d
Zachary Kotlarek <zach@kotlarek.com>
2014-11-03 23:44:14 -0800

    These were handy, but the DB-based system is better even against stdio

--- backup/remoteDir.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -z "${BASE_REMOTE}" ]; then
-	BASE_REMOTE="/mnt/remote/opendrive"
-	if [ ! -d `dirname "${BASE_REMOTE}"` ]; then
-		BASE_REMOTE="/Volumes/OpenDrive"
-	fi
-fi
-echo "${BASE_REMOTE}"
