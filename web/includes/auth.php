a1e467ac723c17de33c3aaba858246e0c9b8fbb4
Zachary Kotlarek <zach@kotlarek.com>
2015-01-04 01:54:13 -0800

    Session cookie options

--- web/includes/auth.php
+++ web/includes/auth.php
@@ -4,6 +4,7 @@ $AUTH_ERR = '';
 
 # Always start a session, unless we're on the CLI
 if (php_sapi_name() != 'cli') {
+	#session_set_cookie_params(0, '/tv', $_SERVER['HTTP_HOST'], true, true);
 	session_start();
 }
 
