9618f87e2b26f9bb11c2196a438321f0a69eb58c
btzoo <btzoo@users.noreply.github.com>
2015-01-13 06:28:56 +0000

    Update findTorrent.pl
    Got trackers from http://kickass.so/community/show/best-working-bittorrent-tracker-list/?page=36#post16003740

--- findTorrent.pl
+++ findTorrent.pl
@@ -33,7 +33,7 @@ my $TIMEOUT = 15;
 my $UA      = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A';
 
 # Static tracker list, appended to all magnet URIs
-my @TRACKERS = ('udp://open.demonii.com:1337/announce', 'udp://tracker.publicbt.com:80/announce', 'udp://tracker.openbittorrent.com:80/announce', 'udp://9.rarbg.com:2710/announce');
+my @TRACKERS = ('udp://open.demonii.com:1337/announce', 'udp://tracker.publicbt.com:80/announce', 'udp://tracker.openbittorrent.com:80/announce', 'udp://9.rarbg.com:2710/announce', 'udp://tracker.istole.it:80/announce', 'udp://tracker.btzoo.eu:80/announce', 'udp://coppersurfer.tk:6969/announce');
 
 # Includes
 use URI::Encode qw(uri_encode);
