7f222dcbc542209195e9f7319f3ee109d5427524
Zachary Kotlarek <zach@kotlarek.com>
2020-04-10 19:04:06 -0700

    New sites

--- findTorrent.pl
+++ findTorrent.pl
@@ -2147,7 +2147,7 @@ sub initSources() {
 
 	# The Pirate Bay
 	if ($CONFIG->{'SOURCES'}->{'TPB'}) {
-		my @proxies = ('thepiratebay.org/search/', 'tpb.unblocked.co/search/', 'bay.xarf.net/search/');
+		my @proxies = ('piratebay.party/search/', 'ahoytpb.art/s/', 'tpbpirate.org/s/');
 		my $source = findProxy(\@proxies, '\bPirate Search\b');
 		if ($source) {
 			$source->{'weight'}         = 1.00;
